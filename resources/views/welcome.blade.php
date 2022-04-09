ff39984010aac2c50ac6e98e6eed6df6643bfffc
Icaro Apolo <apoloraci@gmail.com>
2022-04-09 12:07:30 -0300

    salvando datas no banco

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 
-@section('title', 'HDC Events')
+@section('title', 'Dev Events')
 
 @section('content')
 
@@ -12,19 +12,22 @@
 </div>
 <div id="events-container" class="col-md-12">
     <h2>Eventos em Destaque</h2>
-    <p class="subtitle">Veja os Eventos dos Próximos dias</p>
+    <p class="subtitle">Veja os Eventos dos Próximos Dias</p>
     <div id="cards-container" class="row">
         @foreach($events as $event)
         <div class="card col-md-3">
             <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
             <div class="card-body">
-                <p class="card-date">10/09/2020</p>
+                <p class="card-date">{{ date('d-m-Y', strtotime($event->date)) }}</p>
                 <h5 class="card-title">{{ $event->title }}</h5>
                 <p class="card-participants">X Presenças Confirmadas</p>
                 <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber Mais</a>
             </div>
         </div>
         @endforeach
+        @if(count($events) == 0)
+            <p>Não há eventos disponíveis até o momento</p>
+        @endif
     </div>
 </div>
 
