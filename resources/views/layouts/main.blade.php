d91c4bd771989851d99e77d96939726c4ae12a57
Icaro Apolo <apoloraci@gmail.com>
2022-04-12 21:54:56 -0300

    finalizando layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -69,7 +69,13 @@
         </div>
       </main>
       <footer>
-        <p>Ícaro Apolo &copy; 2022</p>
+          <div>
+              <a href="https://www.linkedin.com/in/icaroapolo/"><ion-icon name="logo-linkedin" class="icons"></ion-icon></a>
+              <a href="https://github.com/IcaroApoloBR"><ion-icon name="logo-github" class="icons"></ion-icon></a>
+              <a href="https://www.instagram.com/icaro.apolo/"><ion-icon name="logo-instagram" class="icons"></ion-icon></a>
+              <a href="https://www.youtube.com/channel/UCDoFiMhpOnLFq1uG4RL4xag"><ion-icon name="logo-youtube" class="icons"></ion-icon></a>
+          </div>
+          <p>Ícaro Apolo &copy;</p>
       </footer>
       <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
       <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
